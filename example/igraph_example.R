ce9cfe2290fa9a5feea06b8d53ad51dd9722740f
stvkerr <steven.miles.k@gmail.com>
2023-10-16 11:07:09 +0200

    Add example scripts

--- example/igraph_example.R
+++ example/igraph_example.R
@@ -1,10 +1,19 @@
-library(igraph)
+pacman::p_load(
+  here, 
+  igraph
+  )
 
+graph <- read.graph(here("data/network.gml"), format = "gml")
 
-graph <- read.graph("ahmedabad.gml", format = "gml")
+saveRDS(graph, here("data/network.rds"))
+graph2 <- readRDS(here("data/network.rds"))
 
-plot(graph, layout = layout_with_fr(graph), vertex.size = 1, vertex.label = NA)
+#plot(graph, layout = layout_with_fr(graph), vertex.size = 1, vertex.label = NA)
+
+str(graph)
 
 degree_dist <- degree_distribution(graph)
 
 hist(degree_dist, main = "Degree Distribution", xlab = "Degree", ylab = "Frequency", col = "blue", breaks = 30)
+
+summary(degree_dist)
